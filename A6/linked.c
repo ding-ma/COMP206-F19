b5c238ea7da2952a616b00ff5b9820f7fd2a2a4c
Ding Ma <ding.ma@mail.mcgill.ca>
2019-11-24 21:32:40 -0500

    modified linked list, there was a bug. everything works now

--- A6/linked.c
+++ A6/linked.c
@@ -3,6 +3,7 @@
 // By Mairead Maloney 2019-11-23
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "linked.h"
 
 
@@ -10,19 +11,26 @@ struct ACCOUNT *head = NULL;
 
 
 /**
- * takes a given account: if account balance does not equal the amount,
- * @param head: head of the list / the current node
- * @param account : account number, as an integer, used to identify accounts
- * @param amnt : transaction ammount from ssv file, used to update account balance
+ * check if the account is in the linked list
+ * if it is, update the amount and return true
+ * else, return false
+ * @param account : account number
+ * @param amount : amount to be updated
+ * @return : true if the account is in the list, false otherwise
  */
-void update(struct ACCOUNT *current, float amount){
-    if(current->balance == amount){
-        return;
+bool isAccountInList(int account, float amount) {
+    struct ACCOUNT *temp;
+    temp = head;
+    bool isAccountUpdated = false;
+    while (temp != NULL) {
+        //while traversing the linked list, if the node matches the account number, update it with the amount
+        if (temp->accountNumber == account) {
+            temp->balance += amount;
+            isAccountUpdated = true;
+        }
+        temp = temp->next;
     }
-    else{
-        current->balance += amount;
-        return;
-    }
+    return isAccountUpdated;
 }
 
 /**
@@ -54,28 +62,6 @@ struct ACCOUNT* addNode(struct ACCOUNT *head, int account, float amount){
     return temp;
 }
 
-/**
- * goes through linked list starting at head, returns node containing the account number
- * @param head: head of the list / the current node
- * @param account : account number, as an integer, used to identify accounts
- * @param amount : transaction ammount from ssv file, used to update account balance
- * @return: returns a struct of type ACCOUNT, that will take the place at the head of the account list
- */
-struct ACCOUNT* findNode(struct ACCOUNT *head, int account, float amount){
-    // Base case
-    if (head == NULL){
-        return addNode(head, account, amount);
-
-    }
-    // If account number is present in current node, return that node
-    if (head->accountNumber == account){
-        return head;
-    }
-
-    // Recur for remaining list
-    return findNode(head->next, account, amount);
-}
-
  /**
  * finds a given node in the linked list of ACCOUNT structs,
  * updates it with new amount value using account
@@ -84,9 +70,9 @@ struct ACCOUNT* findNode(struct ACCOUNT *head, int account, float amount){
  * @param amount : transaction ammount from ssv file, used to update account balance
  */
 void findUpdate(int account, float amount){
-    struct ACCOUNT *node = (struct ACCOUNT *)malloc((sizeof(struct ACCOUNT)));
-    node = findNode(head,account,amount);
-    update(node, amount);
+    if (!isAccountInList(account, amount)) {
+        head = addNode(head, account, amount);
+    }
 }
 
 
@@ -97,9 +83,8 @@ void findUpdate(int account, float amount){
 void prettyPrint(){
     struct ACCOUNT *temp = head;
     while(temp!= NULL){
-        printf("ACCOUNT ID: %d BALANCE: $ %f \n", temp->accountNumber, temp->balance);
+        printf("ACCOUNT ID: %-3d BALANCE: $ %f \n", temp->accountNumber, temp->balance);
         temp = temp->next;
 
     }
-    //
 }
