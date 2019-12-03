d94f62971d04974bbd1768ea5e9eaf3a5515a462
Ding Ma <ding.ma@mail.mcgill.ca>
2019-12-02 22:09:54 -0500

    everything works, add some comments

--- A7/addgold.c
+++ A7/addgold.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,32 +5,28 @@ int main() {
     FILE *fp = fopen("index.html", "r");
     char buffer[1000];
     
-   // char gold[3];
     int goldpieces;
     char *data = getenv("QUERY_STRING");
-    
     printf("Content-Type:text/html\n\n");
     
-    printf("%s", data);
     sscanf(data, "gold=%d", &goldpieces);
- //   int goldpieces = atoi(gold);
     
     if (fp == NULL) {
         perror("Error opening file");
         return (-1);
     }
     
-    int formLineNumber =12;
-    int n =1;
+    int formLineNumber = 12;
+    int n = 1;
     while (fgets(buffer, 999, fp)) {
-        if(n != formLineNumber){
-            printf("%s",buffer);
-        } else{
+        if (n != formLineNumber) {
+            printf("%s", buffer);
+        } else {
             printf("<form action = \"https://www.cs.mcgill.ca/~dma24/cgi-bin/answer.cgi\" method = get>");
             printf("<input type=\"hidden\" name=\"gold\"value=\"%d\">", goldpieces);
         }
         n++;
     }
-
+    
     return 0;
 }
--- A7/answer.c
+++ A7/answer.c
@@ -4,30 +4,18 @@
 
 int main() {
     char input[100];
-   // char *gold; //assumes 10 gold, otherwise it will be over written by the sscanf
     char *data = getenv("QUERY_STRING");
     int goldPieces;
-    /*
-    * Compile this way: gcc answer.c -o answer.cgi
-    * make sure answer.c is in ~/public_html/cgi-bin
-    */
     printf("Content-Type:text/html\n\n");
     printf("<html>");
-    // sscanf(data,"answer=%s=gold=%s",input,gold);
     
     if (strstr(data, "gold") == NULL) {
         sscanf(data, "answer=%s", input);
-        goldPieces =10;
+        goldPieces = 10;
         printf("<input type=\"hidden\" name\"gold\" value=\"10\">");
     } else {
-        sscanf(data, "gold=%d&answer=%s",  &goldPieces,input);
+        sscanf(data, "gold=%d&answer=%s", &goldPieces, input);
     }
-    
-  //  goldPieces = atoi(gold);
-    printf("<p>raw data: %s</p>", data);
-    printf("<p>string input: %s</p>",input);
-  //  printf("<p>%s</p>",gold);
-    printf("<p>gold: %d</p>",goldPieces);
     
     if (strcmp(input, "NORTH") == 0) {
         printf("<head><title>North</title></head>");
@@ -50,24 +38,26 @@ int main() {
         printf("<a href=\"https://www.cs.mcgill.ca/~dma24/cgi-bin/addgold.cgi?gold=%d\">Go Back</a>", goldPieces);
     } else {
         if (strcmp(input, "HelloHi") == 0) {
-            goldPieces+=10;
-            if (goldPieces>= 100) {
+            goldPieces += 10;  //add 10
+            if (goldPieces >= 100) {
                 printf("<p>You Won the Game!!!</p>");
             } else {
-                //add 10
                 printf("<head><title>Good Answer</title></head>");
-                printf("<p>You Win</p>");
+                printf("<p>You Win!</p>");
+                printf("<p>You now have %d gold.</p>", goldPieces);
                 printf("<a href=\"https://www.cs.mcgill.ca/~dma24/cgi-bin/addgold.cgi?gold=%d\">Go Back</a>",
                        goldPieces);
             }
         } else {
-            goldPieces-=5;
-            if (goldPieces<= 0) {
+            goldPieces -= 5;  //subtract 5
+            if (goldPieces <= 0) {
                 printf("<p>You Lost</p>");
                 printf("<p>Ran out of pieces to play</p>");
-            } else {    //substract 5
+                printf("<a href=\"https://www.cs.mcgill.ca/~dma24\">Click here to restart</a>");
+            } else {
                 printf("<head><title>Wrong Answer</title></head>");
                 printf("<p>You Lost</p>");
+                printf("<p>You now have %d gold.</p>", goldPieces);
                 printf("<a href=\"https://www.cs.mcgill.ca/~dma24/cgi-bin/addgold.cgi?gold=%d\">Go Back</a>",
                        goldPieces);
             }
